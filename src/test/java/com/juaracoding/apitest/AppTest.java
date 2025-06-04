762a3634d1c396d4fab18e4560bbc832cfa6c547
Andreas Prasetya <andreasprasetya20@gmail.com>
2025-06-04 21:07:00 +0700

    test

--- src/test/java/com/juaracoding/apitest/AppTest.java
+++ src/test/java/com/juaracoding/apitest/AppTest.java
@@ -1,7 +1,6 @@
 package com.juaracoding.apitest;
 
 import org.hamcrest.CoreMatchers;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -24,12 +23,12 @@ public class AppTest {
         RestAssured.given().header("Authorization", "Token e15f90cdb341ec965f4203fc25cdc71d2d6392db")  .get("/catalogs/groups/")
                 .then().statusCode(200).statusLine("HTTP/1.1 200 OK").body("count", CoreMatchers.instanceOf(Integer.class));
 
-
 //        requestSpecification.header("Authorization", "Token e15f90cdb341ec965f4203fc25cdc71d2d6392db");
 //        response = requestSpecification.get("/catalogs/groups/");
 //        String statusLine = response.statusLine();
 //        int statusCode = response.statusCode();
 //        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
 //        Assert.assertEquals(statusCode, 200);
+
     }
 }
