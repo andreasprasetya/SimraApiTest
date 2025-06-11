3b79a14dbe7f4f339fdb1c5b5364735e6ed696ba
Andreas Prasetya <andreasprasetya20@gmail.com>
2025-06-11 18:36:49 +0700

    test

--- src/test/java/com/juaracoding/apitest/runners/Runner.java
+++ src/test/java/com/juaracoding/apitest/runners/Runner.java
@@ -5,11 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "@Registration", features = {
         "src/test/resources/features/01_Auth.feature"
-},
-        plugin = { "pretty:target/pretty.txt",
-                "html:target/cucumber-reports/index.html",
-                "json:target/cucumber-reports/index.json" },
-        glue = "com.juaracoding.apitest.definitions")
+}, plugin = { "pretty:target/pretty.txt",
+        "html:target/cucumber-reports/index.html",
+        "json:target/cucumber-reports/index.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+}, glue = "com.juaracoding.apitest.definitions")
 public class Runner extends AbstractTestNGCucumberTests {
 
 }
